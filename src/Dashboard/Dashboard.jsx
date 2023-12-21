1b7a84af71f89f24c705ed86f7e729ad760b8584
Noob-Ovi <abdullah.ovi098@gmail.com>
2023-12-22 00:04:15 +0600

    Added user profile

--- src/Dashboard/Dashboard.jsx
+++ src/Dashboard/Dashboard.jsx
@@ -45,15 +45,15 @@ const Dashboard = () => {
     return (
         <div className="drawer lg:drawer-open ">
             <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-            <div className="drawer-content flex flex-col items-center justify-center  h-screen">
+            <div className="drawer-content flex flex-col h-screen lg:overflow-y-scroll ">
               
                 <Outlet></Outlet>
                 <label  htmlFor="my-drawer-2" className="btn absolute top-7 right-7 btn-primary drawer-button lg:hidden "><i className="fa-solid fa-bars"></i></label>
 
             </div>
-            <div className="drawer-side">
+            <div className="drawer-side ">
                 <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label>
-                <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+                <ul className="menu w-80 min-h-full bg-base-200 text-base-content p-8">
                     
                    {
                     menu
--- src/Dashboard/Userhome.jsx
+++ src/Dashboard/Userhome.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import { useContext } from 'react';
+import { authContext } from '../AuthProvider/AuthProvider';
 
 const Userhome = () => {
+    const { user } = useContext(authContext)
     return (
-        <div>
-            userhome
+        <div className='space-y-3 text-center mt-20 w-[80%] mx-auto'>
+            <div className='mb-8'>
+            <img className='w-[100px] h-[100px] rounded-full mx-auto' src={user?.photoURL} alt="profile_picture" />
+            <h1 className='text-lg font-medium'>{user?.displayName}</h1>
+            </div>
+            <div className='grid grid-cols-1 lg:grid-cols-3 gap-5 '>
+                <div className="flex border-2 shadow-md flex-col items-center gap-3 px-8 py-10 bg-white rounded-3xl shadow-main">
+                    <h3 >To-Do:0</h3>
+                </div>
+                <div className="flex border-2 shadow-md flex-col items-center gap-3 px-8 py-10 bg-white rounded-3xl shadow-main">
+                    <h3 >Ongoing:0</h3>
+                </div>
+                <div className="flex border-2 shadow-md flex-col items-center gap-3 px-8 py-10 bg-white rounded-3xl shadow-main">
+                    <h3 >Completed:0</h3>
+                </div>
+                
+            </div>
         </div>
     );
 };
